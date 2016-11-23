57fe271c0fa2d1717342bf4257c0a741d217033b
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-11-23 19:10:02 +0100

    update:  - update version babel-plugin-transform-class-properties  - add babel-plugin-app-decorators-component-register + configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import transformAppDecComponent from "babel-plugin-app-decorators-component";
+import transformAppDecComponentRegister from 'babel-plugin-app-decorators-component-register';
 import transformDecoratorsLegacy from "babel-plugin-transform-decorators-legacy";
 import transformClassProperties from "babel-plugin-transform-class-properties";
 import transformFunctionBind from "babel-plugin-transform-function-bind";
@@ -6,6 +7,13 @@ import transformFunctionBind from "babel-plugin-transform-function-bind";
 export default {
     plugins: [
         transformAppDecComponent,
+        [transformAppDecComponentRegister, {
+            "storage_pointer": '@component',
+            "imports": [
+                { "IMPORT_NAME": "Register", "SOURCE": "app-decorators-helper/register-customelement" },
+                { "IMPORT_NAME": "storage",  "SOURCE": "app-decorators-helper/random-storage" },
+            ],
+        }],
         transformDecoratorsLegacy,
         transformClassProperties,
         transformFunctionBind,
