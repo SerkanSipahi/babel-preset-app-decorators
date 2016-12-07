2219e7fea72c97879f6cdecd35b02aaa2e8398f0
Serkan Sipahi <serkan.sipahi@yahoo.de>
2016-12-07 21:29:47 +0100

    Add babel-plugin-app-decorators-view-precompile and update package version to 1.0.0-beta.2

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import transformAppDecComponent from "babel-plugin-app-decorators-component";
 import transformAppDecComponentRegister from 'babel-plugin-app-decorators-component-register';
+import transformAppDecViewPrecompiler from 'babel-plugin-app-decorators-view-precompile';
 import transformDecoratorsLegacy from "babel-plugin-transform-decorators-legacy";
 import transformClassProperties from "babel-plugin-transform-class-properties";
 import transformFunctionBind from "babel-plugin-transform-function-bind";
@@ -13,6 +14,9 @@ export default {
                 { "IMPORT_NAME": "Register", "SOURCE": "app-decorators-helper/register-customelement" },
                 { "IMPORT_NAME": "storage",  "SOURCE": "app-decorators-helper/random-storage" },
             ],
+        }],
+        [transformAppDecViewPrecompiler, {
+            "engine": "handlebars"
         }],
         transformDecoratorsLegacy,
         transformClassProperties,
